3e1da4ddf65fabacbd851d3b2099699ab925f3f6
ashendilshan <ashenlearn1@gmail.com>
2022-12-30 18:55:06 +0530

    Add intent to Chat2Activity

--- app/src/main/java/com/example/givitapp/RequestActivity.java
+++ app/src/main/java/com/example/givitapp/RequestActivity.java
@@ -7,10 +7,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.example.givitapp.databinding.ActivityChat2Binding;
 
 public class RequestActivity extends AppCompatActivity {
 
     Button map;
+    ImageView chat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +22,20 @@ public class RequestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_request);
 
         map=findViewById(R.id.mapView);
+        chat=findViewById(R.id.messageIcon);
 
         map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(),MapActivity.class);
+                startActivity(i);
+            }
+        });
+
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), ActivityChat2Binding.class);
                 startActivity(i);
             }
         });
